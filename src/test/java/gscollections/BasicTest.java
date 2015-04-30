ab2e47c58f8b93be3a2c1c01d27064d13c42202c
tadashi-aikawa <syou.maman@gmail.com>
2015-05-01 00:31:00 +0900

    GS collectionのテストを追加

--- src/test/java/gscollections/BasicTest.java
+++ src/test/java/gscollections/BasicTest.java
@@ -1,5 +1,7 @@
 package gscollections;
 
+import com.gs.collections.api.LazyIterable;
+import com.gs.collections.api.RichIterable;
 import com.gs.collections.api.list.MutableList;
 import com.gs.collections.api.multimap.Multimap;
 import com.gs.collections.api.multimap.list.ImmutableListMultimap;
@@ -13,7 +15,9 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class BasicTest {
 
@@ -46,6 +50,67 @@ public class BasicTest {
                 .select(e -> e % 2 == 0)
                 .groupBy(e -> e / 10);
         assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 全て一致するかの判定() {
+        assertTrue(
+                Lists.mutable.of(true, true, true)
+                        .allSatisfy(e -> e));
+        assertFalse(
+                Lists.mutable.of(true, true, false)
+                        .allSatisfy(e -> e));
+    }
+
+    @Test
+    public void いずれかが一致するかの判定() {
+        assertTrue(
+                Lists.mutable.of(true, true, false)
+                        .anySatisfy(e -> e));
+        assertFalse(
+                Lists.mutable.of(false, false, false)
+                        .anySatisfy(e -> e));
+    }
+
+    @Test
+    public void Collectionを結合する() {
+        String actual = Lists.mutable.of(1, 3, 5, 7, 9)
+                .makeString("---");
+        assertThat(actual, is("1---3---5---7---9"));
+    }
+
+    @Test
+    public void 条件を満たす要素の数を数える() {
+        int actual = Lists.mutable.of(1, 3, 5, 7, 9)
+                .count(e -> e > 3);
+        assertThat(actual, is(3));
+    }
+
+    @Test
+    public void 最初と最後を取得する() {
+        MutableList<Integer> targets = Lists.mutable.of(1, 3, 5, 7, 9);
+
+        assertThat(targets.getFirst(), is(1));
+        assertThat(targets.getLast(), is(9));
+    }
+
+    @Test
+    public void 最大と最小を取得する() {
+        MutableList<Integer> targets = Lists.mutable.of(1, 3, 5, 7, 9);
+
+        assertThat(targets.max(), is(9));
+        assertThat(targets.min(), is(1));
+    }
+
+    @Test
+    public void 一定数で区切る() {
+        LazyIterable<RichIterable<Integer>> actual = Interval.fromTo(1, 9).chunk(3);
+
+        assertThat(actual, contains(
+                Lists.mutable.of(1, 2, 3),
+                Lists.mutable.of(4, 5, 6),
+                Lists.mutable.of(7, 8, 9)
+        ));
     }
 
 }
